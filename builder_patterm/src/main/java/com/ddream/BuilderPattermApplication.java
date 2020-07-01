282dbd01a6a0a9039799a90bcb7d95d4fad8674d
xunyong <wangxy@hifangjia.com>
2020-07-01 11:45:39 +0800

    构建模式

---
+++ builder_patterm/src/main/java/com/ddream/BuilderPattermApplication.java
@@ -0,0 +1,13 @@
+package com.ddream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuilderPattermApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BuilderPattermApplication.class, args);
+    }
+
+}
---
+++ builder_patterm/src/test/java/com/ddream/BuilderPatternApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ddream;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = BuilderPattermApplication.class)
+class BuilderPatternApplicationTests {
+
+    @Test
+    void contextLoad(){ }
+
+
+}
